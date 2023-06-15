46bef01e3833be069106f6cd869a09244fefbb47
Jerome A <je.aprile@gmail.com>
2023-06-15 15:28:35 +0200

    Commit des solutions aux exos 3 à 6

--- test.js
+++ test.js
@@ -1,30 +1,273 @@
 const readlineSync = require("readline-sync");
 
+/* Exercice 6.4
 
-function findMinMax(array) {
-    let min = array[0];
-    let max = array[0];
+class Personne {
+  constructor(firstname, lastname) {
+    this._firstname = firstname;
+    this._lastname = lastname;
+  }
+
+  get name() {
+    return `${this._firstname} ${this._lastname}`;
+  }
+
+  set name(fullname) {
+    const [firstname, lastname] = fullname.split(" ");
+    this._firstname = firstname;
+    this._lastname = lastname;
+  }
+}
+
+let personne = new Personne("John", "Doe");
+console.log(personne.name); 
+
+personne.name = "Jane Smith";
+console.log(personne.name); 
+console.log(personne); 
+
+
+/* Exercice 6.3
+
+class Animal {
+
+constructor(name, greeting, species) {
+  this.name = name;
+  this.greeting = greeting;
+  this.species = species;
+}
+
+sayHello() 
+  { 
+    return this.greeting + "! My name is " + this.name + ", and I am a " + this.species + "!";
+  }
+}
+
+let Cat = new Animal ("Whiskers", "Meow", "Cat");
+let Dog = new Animal ("Buddy", "Woof", "Dog");
+let Parrot = new Animal ("Blue", "Coco", "Parrot");
+
+
+console.log(Cat.sayHello());
+console.log(Dog.sayHello());
+console.log(Parrot.sayHello());
+*/
+
+
+/*
+Exercice 6.2
+
+class Person {
+  constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+  }
+
+  sayHello() {
+      return `Hello, my name is ${this.firstname} ${this.lastname}!`;
+  }
+}
+
+let human1 = new Person("John", "Doe");
+let human2 = new Person("Thomas", "Gendebien");
+
+
+console.log(human2.sayHello());
+*/
+
+/*
+Exercice 6.1
+
+class Cat {
+  constructor(name, age) {
+      this.name = name;
+      this.age = age;
+  }
+}
+
+let skitty = new Cat("Skitty", 9);
+let pixel = new Cat("Pixel", 6);
+
+console.log(skitty);
+console.log(pixel);
+*/
+
+/* Exercice 5.2
+
+let serieName = "";
+let productionYear = "";
+let castMembers = "";
+let cast = [];
+let TVSerie = {"name": serieName, "productionYear": productionYear, "cast": cast};
+
+
+function askTVseries() {
+  serieName = readlineSync.question("Can you please give me the name of your favorite tv show?");
+  productionYear = readlineSync.question("Oh, that's avery good one ! What's de production year ?");
+
+  function askCastMember() {
+    let member = readlineSync.question("Nom d'un membre du casting (ou 'fin' pour terminer) : ")
+      if (member.toLowerCase() === 'fin') {
+      return;      
+      } else {
+        cast.push(member);
+        askCastMember();
+      }};
+
+
+  askCastMember();
+
+
+  console.log(JSON.stringify(TVSerie));
+
+}
   
-    for (let i = 1; i < array.length; i++) {
-      if (array[i] < min) {
-        min = array[i];
-      }
+function randomizeCast(TVSerie) {
+  const { name, year, cast } = TVSerie;
+  const randomizedCast = [...cast].sort(() => Math.random() - 0.5);
+  let altTVSerie = {"name": serieName, "productionYear": productionYear, "cast": randomizedCast};
+
+   return  console.log(JSON.stringify(altTVSerie));
+}
+
+randomizeCast(TVSerie);
+*/
+
+
+/* Exercice 5.1
+
+
+let serieName = "";
+let productionYear = "";
+let castMembers = "";
+let cast = [];
+let TVSerie = {"name": serieName, "productionYear": productionYear, "cast": cast};
+
+
+function askTVseries() {
+  serieName = readlineSync.question("Can you please give me the name of your favorite tv show?");
+  productionYear = readlineSync.question("Oh, that's avery good one ! What's de production year ?");
+
+  function askCastMember() {
+    let member = readlineSync.question("Nom d'un membre du casting (ou 'fin' pour terminer) : ")
+      if (member.toLowerCase() === 'fin') {
+      return;      
+      } else {
+        cast.push(member);
+        askCastMember();
+      }};
+
+
+  askCastMember();
+
+
+  console.log(JSON.stringify(TVSerie));
+
+}
   
-      if (array[i] > max) {
-        max = array[i];
-      }
-    }
-  
-    return { min, max };
+askTVseries();
+*/
+
+
+
+/* Exercice 4.6
+
+let arr = [];
+
+function factorial(a) {
+  for(i=1; i<=a; i++) {
+    arr.push(i)
   }
   
-  const array = [5, 8, 2, 4, 1, 9, 3];
-  const result = findMinMax(array);
+
+let sum = 1;
+let size = arr.length;
+
+  for(let i = 0; i < size; i++) {
+    sum *= arr[i]; 
+  } 
+
+  return console.log(sum);
+
+}
+
+factorial(8);
+
+*/
+
+/* Exercice 4.5
+
+function calcDistance(pointA, pointB) {
+  let [x1, y1] = pointA;
+  let [x2, y2] = pointB;
+
+  const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  return distance.toFixed(2);
+
+}
+
+let pointA = [4, 3];
+let pointB = [3, 4];
+
+let distance = calcDistance(pointA, pointB);
+
+console.log(distance); 
+*/
+
+/* Exercice 4.4
+
+function rand10() {
+  return Math.floor(Math.random() * 20) + 1;
+}
+
+let n = parseInt(readlineSync.question("Can you please give me a number ?"));
+
+let arr = [];
+
+for(let i=0; i < n; i++) {
+  arr.push(rand10())
+}
+
+console.log(arr);
+
+function average(arr) {
+
+  let size = arr.length;
+  let sum = 0;
   
-  console.log("Array:", array);
-  console.log("Minimum:", result.min);
-  console.log("Maximum:", result.max);
-  
+  for(let i = 0; i < size; i++) {
+    sum += arr[i]; 
+  } 
+
+  sum /= arr.length
+
+  return console.log(sum);
+
+} 
+
+function min(arr) {
+  if (!arr || arr.length === 0) {
+    return null;
+  }
+  return console.log(Math.min(...arr));
+}
+
+function max(arr) {
+  if (!arr || arr.length === 0) {
+    return null;
+  }
+  return console.log(Math.max(...arr));
+}
+
+average(arr);
+
+min(arr);
+
+max(arr);
+
+*/
+
+
 
 /* Exercice 3.3
 
@@ -59,6 +302,95 @@ slice() method
   console.log("Duplicated array (using slice()):", duplicatedArray2);
   
   */
+
+
+/* Exercices 3.2
+let arr = [100, 101, 102]
+
+let size = arr.length;
+let sum = 0;
+
+for(let i = 0; i < size; i++) {
+  sum += arr[i]; 
+} 
+
+sum /= arr.length
+
+console.log(sum);
+*/
+
+
+
+/* Exercice 3.1
+let arr = [100, 101, 102]
+
+let size = arr.length;
+let sum = 0;
+
+for(let i = 0; i < size; i++) {
+  sum += arr[i]; 
+} 
+
+console.log(sum);
+
+*/
+
+
+
+
+
+
+
+
+/* Exercice 4.3
+
+function rand10() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+ 
+function multiRand(n) {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(rand10());
+  }
+  return result;
+}
+
+let quantity = parseInt(readlineSync.question("Enter the number of random numbers to generate:"));
+let randomNumbers = multiRand(quantity);
+console.log(randomNumbers);
+*/
+
+
+/* Exercice 4.2
+
+function rand10() {
+  return Math.floor(Math.random() * 10) + 1;
+}
+
+let random = rand10();
+
+console.log("The random number between 1 and 10 is: " + random + " !");
+*/
+
+
+
+/* Exercice 4.1
+
+function calcularray (nombre1, nombre2) {
+  return nombre1*nombre2;
+}
+
+let longueur = parseInt(readlineSync.question("Quelle est la longeur de ton rectangle ?"))
+let largeur = parseInt(readlineSync.question("Et quelle en est la largeur ?"))
+
+ let array = calcularray (longueur, largeur)
+
+ console.log("Alors, l'air de ton rectangle est de : " + array + " !")
+*/
+
+
+
 
 
 /* Exercice 3.2
@@ -105,23 +437,20 @@ function sumArrayElements(array) {
 
 
 
-
 /* Exercice 2.8
 
-const readlineSync = require('readline-sync');
 
-let n = parseInt(readlineSync.question("Entrez un nombre n : "));
-let sum = 0;
+let n = parseInt(readlineSync.question("Can you please give me a number ?"))
+let sum = 0
 
 for (let i = 0; i < n; i++) {
-  let number = parseFloat(readlineSync.question(`Entrez le nombre ${i + 1} : `));
-  sum += number;
+ let number = parseInt(readlineSync.question("Enter a new number !"))
+  sum += number
 }
 
-console.log("La somme des nombres est :", sum);
+console.log("La somme des " + n + " nombres que tu m'as donnés est : " + sum + " !")
+
 */
-
-
 
 /* Exercice 2.7
 
@@ -162,19 +491,15 @@ console.log("Le nombre", number, "correspond au jour de la semaine", day + ".");
 
 /* Exercice 2.6
 
-let favoriteNumber;
+let favoriteNumber = readlineSync.question("Quel est votre nombre préféré ? ");
 
-while (favoriteNumber !== 42) {
-  favoriteNumber = readlineSync.question("Quel est votre nombre préféré ? ");
-
-  if (favoriteNumber !== '42') {
+while (favoriteNumber != "42") {
     console.log("Êtes-vous sûr ?");
+    favoriteNumber = readlineSync.question("Quel est votre nombre préféré ? ");
   }
-}
 
-console.log("Félicitations ! Vous avez choisi le nombre 42.");
+ console.log("Félicitations ! Vous avez choisi le nombre 42.");
 */
-
 
 
 /* Exercice 2.5
